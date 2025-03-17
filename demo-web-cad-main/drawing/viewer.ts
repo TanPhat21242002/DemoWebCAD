dd8252725beb32c9d218a118f3424add901713dd
Tấn Phát <tanphat212420@gmail.com>
2025-03-17 08:24:01 +0700

    [Proj_P1] delete event wheel

--- demo-web-cad-main/drawing/viewer.ts
+++ demo-web-cad-main/drawing/viewer.ts
@@ -183,35 +183,9 @@ export class ViewerIns {
 			ev.preventDefault();
 		});
 
-		this.viewer.addEventListener("wheel", ev => {
-			this.handleMouseWheel(ev);
-		});
-
 		document.onkeydown = ev => {
 			this.fireSubcribeEvent(VIEWER_EVENT.KEY_PRESS, ev, ev.keyCode);
 		};
-	}
-
-	private handleMouseWheel(ev: WheelEvent) {
-		ev.preventDefault();
-		const zoomFactor = 1.3;
-		const minZoom = 50;
-		const maxZoom = 1500;
-		const viewWidth = this.visViewer.activeView.viewFieldWidth;
-
-		if (ev.deltaY < 0 && viewWidth <= minZoom) {
-			return;
-		} else if (ev.deltaY > 0 && viewWidth >= maxZoom) {
-			return;
-		}
-
-		if (ev.deltaY < 0) {
-			this.visViewer.zoomAt(zoomFactor, this.canvas.width / 2, this.canvas.height / 2);
-		} else {
-			this.visViewer.zoomAt(1 / zoomFactor, this.canvas.width / 2, this.canvas.height / 2);
-		}
-
-		this.fireSubcribeEvent(VIEWER_EVENT.ZOOM_CHANGED, ev, zoomFactor);
 	}
 
 	private startPan(ev) {
